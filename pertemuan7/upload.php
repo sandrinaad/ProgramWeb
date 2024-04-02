c22e8b29cd632a5ea7e7e6cb115b2aa885616454
sandrinaad <sandrina.bridge@gmail.com>
2024-04-02 09:54:26 +0700

    Pertemuan 8

--- pertemuan7/upload.php
+++ pertemuan7/upload.php
@@ -1,19 +1,34 @@
 <?php
-if(isset($_POST["submit"])){
-    $targetDirectory = "documents/"; //Direktori tujuan untuk menyimpan file
-    $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
-    $documentFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+if (isset($_POST['submit'])) {
+    // Menentukan direktori tujuan untuk menyimpan file yang diunggah
+    $targetDirectory = "documents/";
+    // Menentukan nama file target dengan menggunakan nama file yang diunggah
+    $targetFile = $targetDirectory . basename($_FILES["documentToUpload"]["name"]);
+    $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
+    // Mendefinisikan jenis file yang diizinkan untuk diunggah
     $allowedExtensions = array("txt", "pdf", "doc", "docx");
-    $maxFileSize = 10 * 1024 * 1024;
 
-    if (in_array($documentFileType, $allowedExtensions) && $_FILES["fileToUpload"]["size"] <= $maxFileSize){
-        if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)){
-            echo "Dokumen berhasil diunggah.";
-        }else{
-            echo "Gagal mengunggah dokumen.";
+    // Mendefinisikan ukuran maksimum file yang diizinkan untuk diunggah (dalam bytes)
+    $maxFileSize = 5*1024*1024; // 5 MB
+
+    // Memeriksa apakah jenis file dan ukuran file yang diunggah sesuai dengan yang diizinkan
+    if (isset($_FILES["documentToUpload"]) && $_FILES["documentToUpload"]["error"] == UPLOAD_ERR_OK) {
+        if (in_array($fileType, $allowedExtensions) && $_FILES["documentToUpload"]["size"] <= $maxFileSize) {
+            // Jika sesuai, maka coba untuk memindahkan file ke direktori tujuan
+            if(move_uploaded_file($_FILES["documentToUpload"]["tmp_name"], $targetFile)){
+                echo "Dokumen berhasil diunggah.";
+            } else {
+                // Jika gagal memindahkan file, tampilkan pesan kesalahan
+                echo "Gagal mengunggah dokumen.";
+            }
+        } else {
+            // Jika jenis file tidak sesuai atau ukuran melebihi batas maksimum, tampilkan pesan kesalahan
+            echo "Jenis dokumen tidak valid atau melebihi ukuran maksimum yang diizinkan (5 MB).";
         }
-    }else{
-        echo "jenis dokumen tidak valid atau melebihi ukuran maksimum yang diizinkan.";
+    } else {
+        // Jika ada kesalahan dalam unggahan file, tampilkan pesan kesalahan
+        echo "Terjadi kesalahan dalam unggahan dokumen.";
     }
 }
+?>
